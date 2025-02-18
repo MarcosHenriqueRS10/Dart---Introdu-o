3de8f3e5caca50cbed14b49b2815e5f5227a5146
Marcos Henrique Ribeiro de Santana. <161729448+MarcosHenriqueRS10@users.noreply.github.com>
2025-02-18 11:31:23 -0300

    Add files via upload

---
+++ intro.dart
@@ -0,0 +1,39 @@
+void main() {
+  print('Hello world:');
+
+  // var nome = "Marcos";
+  // String sobrenome = "Santana";
+  // sobrenome.toUpperCase();
+  // Object time = "Corinthians";
+
+  // String linguagem = 'Dart';
+  // print('Você está aprendendo ${linguagem.toUpperCase()}. É muito divertido!');
+  // print('Você está aprendendo' + linguagem +'. É muito divertido!');
+
+  // List<String> linguagens = ["Dart", "Java", "Python"];
+  // linguagens.add("PHP");
+  // print("Tamanho da lista: ${linguagens.length}");
+  // print(linguagens[0]);
+
+  // for (int i = 0; i < linguagens.length; i++) {
+  //   print(linguagens[i]);
+  // }
+
+  // for (String linguagem in linguagens) {
+  //   print(linguagem);
+  // }
+
+  // linguagens.forEach(print);
+
+  // const double pi = 3.14;
+  // pi = 3.15;
+
+  // const agora = DateTime.now();
+  // final data = new DateTime(2025); não precisa do new..
+}
+
+int multiplicar(int a, int b) {
+  return a * b;
+}
+
+int multiplicarArrow(int a, int b) => a * b;
